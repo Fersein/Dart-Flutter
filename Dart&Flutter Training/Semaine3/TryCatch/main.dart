390315cbd9fa551b934da6efab70ef73c3c350ce
Fersein <fersein_cgtech@hotmail.com>
2025-08-07 06:29:40 +0000

    TryCatch

---
+++ Dart&Flutter Training/Semaine3/TryCatch/main.dart
@@ -0,0 +1,9 @@
+import 'dart:async';
+void main() async {
+  try {
+    // await basededonnee;
+  } catch (e) {
+    print("Pas possible d'obtenir les donnees . voici l'erreur ${e.toString}");
+    
+  }
+}
